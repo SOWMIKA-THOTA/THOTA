4417c7fadadb2bd29c3d77519e8e5d947bfd4fea
SOWMIKA-THOTA <41738271+SOWMIKA-THOTA@users.noreply.github.com>
2018-07-27 13:15:39 +0530

    Create program7.java

---
+++ program7.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class program7
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+           BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        int N = Integer.parseInt(line);
+        for (int i = 0; i < N; i++) 
+        {
+            System.out.println("hello");
+        }
+	}
+}
