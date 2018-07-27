228b7692cd68e8f5b655b554d2e2d9b133f8e1db
SOWMIKA-THOTA <41738271+SOWMIKA-THOTA@users.noreply.github.com>
2018-07-27 22:15:01 +0530

    Create program7.java

---
+++ program7.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+class program7
+{
+  public static void main(String args[]) throws java.lang.Exception
+{
+  Scanner input= new Scanner(System.in);
+       long N= input.nextLong();
+       for(int i=0;i<N;i++) 
+           System.out.println("Hello");
+  }
+}
