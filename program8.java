9a44abade6eaa46b5d272916db992cc57836840b
SOWMIKA-THOTA <41738271+SOWMIKA-THOTA@users.noreply.github.com>
2018-07-27 15:28:42 +0530

    Create program8.java

---
+++ program8.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+class program8
+{
+  public static void main(String args[]) throws java.lang.Exception
+  {
+   int number, i, sum = 0;
+   Scanner sc = new Scanner(System.in);
+   number = sc.nextInt();	
+  for(i = 1; i <= number; i++)
+  {
+	sum = sum + i; 
+   }	
+   System.out.println(sum);
+}
+}
