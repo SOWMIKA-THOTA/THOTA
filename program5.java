4582f8f7803a35a78871c3ad7b2810bed88e8bce
SOWMIKA-THOTA <41738271+SOWMIKA-THOTA@users.noreply.github.com>
2018-07-27 12:30:40 +0530

    Create program5.java

---
+++ program5.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class program5
+{
+	public static void main (String[] args) throws java.lang.Exception
+    {
+        int a, b,c;
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the first number:");
+        a= sc.nextInt();
+        System.out.print("Enter the second number:");
+         b=sc.nextInt();
+        System.out.print("Enter the third number:");
+        c=sc.nextInt();
+        if(a>b && a>c)
+        {
+            System.out.println("Largest number is:"+a);
+        }
+        else if(b>c)
+        {
+            System.out.println("Largest number is:"+b);
+        }
+        else
+        {
+            System.out.println("Largest number is:"+c);
+        }
+ 
+    }
+}
