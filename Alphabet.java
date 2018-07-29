691d3700c191ad976029bdbedbcd9d79a438d3c2
SOWMIKA-THOTA <41738271+SOWMIKA-THOTA@users.noreply.github.com>
2018-07-29 15:35:09 +0530

    Create Alphabet.java

---
+++ Alphabet.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Alphabet
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+      char c='A';
+      if ((c>='A'&& c<='Z') || (c>='a'&& c<='z'))
+	    System.out.println("Alphabet");
+      else
+	    System.out.println("No");
+	}
+}
